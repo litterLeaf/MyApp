2f637e18f4b7d35c80b3e6802fe1ce59bfd592f7
Freeder <song123>
2018-12-27 18:54:41 +0800

    上传项目

--- app/src/main/java/com/dell/myapp/MainActivity.java
+++ app/src/main/java/com/dell/myapp/MainActivity.java
@@ -2,12 +2,26 @@ package com.dell.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        service =findViewById(R.id.service);
+        service.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case  R.id.service:
+
+                break;
+        }
     }
 }
